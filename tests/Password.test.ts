ca0fcf4d8ad02c7c3b543e5e5619b5bd1416fda1
KMosunov <kmosunov@gmail.com>
2025-01-13 12:23:02 +0300

    fix: TL-1 Password.test.ts fixed with test names added, test names in .yml files added

--- tests/Password.test.ts
+++ tests/Password.test.ts
@@ -7,88 +7,88 @@ describe("Password tests", () => {
   });
 
   // 2. ‘1234567’ str = 1 Very weak
-  test("1234567", () => {
+  test("7 symbols: digits only", () => {
     expect(calculatePasswordStrength("1234567")).toBe("Very Weak");
   });
 
   // 3. ‘abc1234’ str = 2 Very weak
-  test("abc1234", () => {
+  test("7 symbols: digits and latin lowcase letters", () => {
     expect(calculatePasswordStrength("abc1234")).toBe("Very Weak");
   });
 
   // 4. 'abcабв1' str = 3 Weak
-  test("abcабв1", () => {
+  test("7 symbols: digits, latin and cyrillic lowcase letters", () => {
     expect(calculatePasswordStrength("abcабв1")).toBe("Weak");
   });
 
   // 5. ‘абвабв1’ str = 2 Very weak
-  test("абвабв1", () => {
+  test("7 symbols: digits and cyrillic lowcase letters", () => {
     expect(calculatePasswordStrength("абвабв1")).toBe("Very Weak");
   });
 
   // 6. ‘abcABC1’ str = 3 Weak
-  test("abcABC1", () => {
+  test("7 symbols: digits, latin lowcase and uppercase letters", () => {
     expect(calculatePasswordStrength("abcABC1")).toBe("Weak");
   });
 
   // 7. ‘abAB1!@‘ str = 4 Moderate
-  test("abAB1!@", () => {
+  test("7 symbols: digits, latin lowcase, uppercase letters, special characters", () => {
     expect(calculatePasswordStrength("abAB1!@")).toBe("Moderate");
     //console.log(Password.strength)
   });
 
   // 8. ‘12345678’ str = 2 Very weak
-  test("12345678", () => {
+  test("8 symbols: digits only", () => {
     expect(calculatePasswordStrength("12345678")).toBe("Very Weak");
   });
 
   // 9. ‘123456789’ str = 2 Very weak
-  test("123456789", () => {
+  test("9 symbols: digits only", () => {
     expect(calculatePasswordStrength("123456789")).toBe("Very Weak");
   });
 
   // 10. ‘123456abc’ str = 3 Weak
-  test("123456abc", () => {
+  test("9 symbols: digits and latin lowcase letters", () => {
     expect(calculatePasswordStrength("123456abc")).toBe("Weak");
   });
 
   // 11. ‘123abcABC’ str = 4 Moderate
-  test("123abcABC", () => {
+  test("9 symbols: digits, latin lowcase and uppercase letters", () => {
     expect(calculatePasswordStrength("123abcABC")).toBe("Moderate");
   });
 
   // 12. ‘123abAB!@‘ str = 5 Strong
-  test("123abAB!@", () => {
+  test("9 symbols: digits, latin lowcase, uppercase letters, special characters", () => {
     expect(calculatePasswordStrength("123abAB!@")).toBe("Strong");
   });
 
   // 13. ‘12345678901’ str = 2 Very weak
-  test("12345678901", () => {
+  test("11 symbols: digits only", () => {
     expect(calculatePasswordStrength("12345678901")).toBe("Very Weak");
   });
 
   // 14. ‘123456789012’ str = 3 Weak
-  test("123456789012", () => {
+  test("12 symbols: digits only", () => {
     expect(calculatePasswordStrength("123456789012")).toBe("Weak");
   });
 
   // 15. ‘1234567890123’ str = 3 Weak
-  test("1234567890123", () => {
+  test("13 symbols: digits only", () => {
     expect(calculatePasswordStrength("1234567890123")).toBe("Weak");
   });
 
   // 16. ‘abc1234567890’ str = 4 Moderate
-  test("abc1234567890", () => {
+  test("13 symbols: digits and latin lowcase letters", () => {
     expect(calculatePasswordStrength("abc1234567890")).toBe("Moderate");
   });
 
   // 17. ‘abcABC1234567’ str = 5 Strong
-  test("abcABC1234567", () => {
+  test("13 symbols:latin lowcase and uppercase letters", () => {
     expect(calculatePasswordStrength("abcABC1234567")).toBe("Strong");
   });
 
   // 18. ‘-@ abcABC1234’ str = 6 Strong
-  test("-@ abcABC1234", () => {
+  test("13 symbols: digits, latin lowcase, uppercase letters, special characters", () => {
     expect(calculatePasswordStrength("-@ abcABC1234")).toBe("Strong");
   });
 });
